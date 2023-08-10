0be1c2ac30988cf836300f950326abb32ae5a06e
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-10 12:33:51 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * This function prints a message using the printf function.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
