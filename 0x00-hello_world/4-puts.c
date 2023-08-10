14a7eed41a647c9a13852f98238327994800dae6
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-10 12:02:41 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /*
- * This is the main function of the program.
- * It prints a message to the console using the puts function.
- * Returns: 0 on successful execution.
+ * main - Entry point
+ * 
+ * return: 0
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
-
-	return (0);
 }
+	return (0);
