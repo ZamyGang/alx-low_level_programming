b714964858dca4538ba6c77c6b08e2b7c706e124
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-11 12:11:30 -0700

    if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
+	else if (n == 0)
+        {
+                printf("Last digit of %d and is %d \n", n, m);
+        }
 	else if (n < 6 && n != 0)
         {
                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
         }
-	else if (n == 0)
-	{
-		printf("Last digit of %d and is %d \n", n, m);
-	}
 	return (0);	
 }
