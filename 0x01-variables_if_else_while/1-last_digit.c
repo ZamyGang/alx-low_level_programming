5c25edf5651c8a87e6acbcff53c78a3b18a69a36
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-11 12:21:09 -0700

    if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (n > 5 && n != 0)
+	if (n == 0 && n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
-	else if (n == 0)
+	else
         {
                 printf("Last digit of %d and is %d \n", n, m);
         }
