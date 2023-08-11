824e3fcc4b049bcc323b4f6a8f7018bdcf62ba86
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-11 14:58:18 -0700

    ïf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char lower = 'a';
+	for (lower = 'a'; lower <= 'z' ; lower++)
+	{
+		if(lower == q || lower == e)
+		{
+			putchar(lower);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
