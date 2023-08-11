c572dcbd19f07673ca2045b3db0a3e5cb30cfb64
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-11 12:29:53 -0700

    if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
         {
                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
         }
-	else if (n == 0 || n = 980)
+	else if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0",n ,m);
 	}
