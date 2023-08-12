ee32d8eca37d0a81990b3f00137d6f48046f8ea2
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-12 05:22:17 -0700

    if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
-int main (void)
+/*
+ * main - entry point
+ * Description: this main function prints alphabets except for q and e.
+ * Return: 0 (Success)
+ */
+int main(void)
 {
 	char lower = 'a';
+
 	for (lower = 'a'; lower <= 'z' ; lower++)
 	{
-		if(lower != 'q' && lower != 'e')
+		if (lower != 'q' && lower != 'e')
 		{
 			putchar(lower);
 		}
