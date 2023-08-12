29913e68bfbbcb47c679a9ddb7b8511600f4d628
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-12 05:28:10 -0700

    if

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
+ * Main - entry point
+ * Description: this function prints the alphabet in reverse
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	char a;
-	
+
 	for (a = 'z'; a >= 'a'; a--)
 	{
 		putchar(a);
