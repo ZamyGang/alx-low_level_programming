5842128fcfde0a48cabc9908aa39e717b8294e1a
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-12 05:03:46 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,11 @@
 #include <time.h>
 /*
  * Main - Entry point
- * Description: A function that prints out negative,positive or zero
- * depending on the output of the variable
+ * This program generates a random integer 'n' and determines its last digit.
+ * It categorizes the digit as greater than 5, equal to 0, or less than 6 but not * 0.
+ * The random number generator is seeded with the current time for randomness.
+ * This program demonstrates the usage of random numbers, arithmetic operations,
+ * and conditionals in C.
  * Return: (0) Success
  *
  */
