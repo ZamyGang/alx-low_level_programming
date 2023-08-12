57c24f8ef96899c61fdaad01cfc63847cf88fa30
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-12 04:49:08 -0700

    if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("and is 0\n" m);
+		printf("and is 0\n");
 	}
 	else
 	{
-		printf(" and is less than 6 and not 0\n", n, m);
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
