6c733717676c8242f8b47dc47a6cddc1ee51b282
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-12 01:19:00 -0700

    if

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,11 @@ int main(void)
 	for (a = 48; a < 58 ; a++)
 	{
 		putchar(a);
-
-		putchar(',');
-		
-		putchar(' ');
+		if(a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
