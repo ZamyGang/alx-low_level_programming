cc5d346f682689625e5bac8a4ace7896f6c1d4af
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-15 07:45:46 -0700

    Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * A function that prints alphabets in lower case
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i >= 122; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+	return (0);
+}
