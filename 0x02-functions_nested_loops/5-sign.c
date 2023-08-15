ff64b77e5f0ddef296ac3e7eac8f864258c28483
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-15 11:22:16 -0700

    Functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
 	else if(n < -1)
 	{
-		printf("-1");
+		_putchar('-1');
 		return (-1);
 	}
 	else
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
 }
