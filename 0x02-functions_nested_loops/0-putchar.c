5d72bd4c215ef936797f649fe03ee578e239e6d4
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-15 05:13:59 -0700

    Functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,8 @@
 #include "main.h"
-/**
- * Main - Entry point
- *
- * A function that prints out -putchar
- *
- * Return: 0 success
- */
+
 int main(void)
 {
-	_putchar(_putchar);
+	_putchar("_putchar");
 
 	return (0);
 }
