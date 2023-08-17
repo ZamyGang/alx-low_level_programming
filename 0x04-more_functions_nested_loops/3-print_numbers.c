3952be927a5193df81c9d9b8c918077430e9e067
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-17 11:01:45 -0700

    functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * print_numbers - print the numbers, from 0 to 9 followed by a new line.
- * 
+ *
  * Return: (0) Success
  */
 void print_numbers(void)
 {
 	int a;
-	
+
 	for (a = 48; a <= 57; a++)
 	{
 		_putchar(a);
 	}
+	_putchar('\n');
 }
