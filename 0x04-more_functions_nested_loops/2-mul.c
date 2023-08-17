6c0ffe148e0274f1a5f0783f58d538d2f767c4e1
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-17 10:45:09 -0700

    function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies to integers.
+ * @a: digit input
+ * @b: digit input
+ * return: (0) success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
