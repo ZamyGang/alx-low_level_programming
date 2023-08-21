a74f2b0d68a7711843113b38d22c72a3183fa6e0
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-21 03:00:46 -0700

    point

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * reset_to_98 - a function that takes a point to an int as parameter and
+ * updates the value it points to 98.
  *
- *
- *
- *
- *
- *
+ * return: 0 (success)
  */
 
 void reset_to_98(int *n)
