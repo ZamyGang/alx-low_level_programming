ac6c67c5c910e21f1c3412019c44908a5231820a
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-30 07:48:20 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -15,9 +15,6 @@ void _print_rev_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	else
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
