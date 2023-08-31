ed6494b14b16a77c0696a2a08c2240a46565c7e6
ZamyGang <gomolemoonline3358@gmail.com>
2023-08-31 15:33:04 -0700

    Task 2 fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,14 @@
 /**
  * _strlen_recursion - A function that returns the length of a string.
  * @s: input
- * @count: input
- * return 0 (success).
+ * Return 0 (success)
  *
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
