5e9cd0fc3860727d192443d385778dff348e8eac
ZamyGang <gomolemoonline3358@gmail.com>
2023-09-05 16:26:34 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *
  *
@@ -8,21 +9,26 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *s;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *array = (char *)malloc(size * sizeof(char));
+	s = malloc((size) * sizeof(char));
 
-	if (array == NULL)
+	if (s == NULL)
 	{
 		return (NULL);
 	}
-
-	for (unsigned int i = 0; i < size; i++)
+	i = 0;
+	while (i < size)
 	{
-		array[i] = c;
+		s[i] = c;
+		i++
 	}
-	return array;
+	s[i] = '\0';
+	return (s);
 }
