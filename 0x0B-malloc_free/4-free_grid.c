482b176cecc3b9fa5f6242fbdd0b6d9512d5552f
ZamyGang <gomolemoonline3358@gmail.com>
2023-09-06 02:59:10 -0700

    Task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *
  *
  *
  */
-void free_grid(int **grid, int height) {
-    if (grid == NULL) {
-        return;
-    }
-
-    for (int i = 0; i < height; i++) {
-        free(grid[i]);
-    }
-
-    free(grid); 
+void free_grid(int **grid, int height)
+{
+	int y;
+	if (grid == NULL || grid == 0)
+	{
+   		return;
+	}
+	for (y = 0; y < height; y++)
+	{
+        	free(grid[y]);
+	}
+	free(grid); 
 }
