71356305f95999be102f636872e6d09a3dede701
ZamyGang <gomolemoonline3358@gmail.com>
2023-09-14 10:54:44 -0700

    Task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,20 +7,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	int a, sum;
-
-	if (n != NULL)
-	{
-		va_start(ap , n);
-		sum = 0;
-		for (a = 0; a < n; a++)
-			sum += va_arg (ap, int);
-
-		va_end (ap);
-	}
-
 	if (n == 0)
 		return (0);
+
+	va_list ap;
+	int a, sum;
+	
+	va_start(ap , n);
+	sum = 0;
+	for (a = 0; a < n; a++)
+		sum += va_arg (ap, int);
+
+		va_end (ap);
 
 }
