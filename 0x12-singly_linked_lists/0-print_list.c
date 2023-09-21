c3e3f97185a038fa85b6cd88440a25205a2d85f2
ZamyGang <gomolemoonline3358@gmail.com>
2023-09-21 14:55:28 -0700

    Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%d] s%\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		node++;
 		h = h->next;
